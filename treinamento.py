dd6b06a26480b7199c77699d18537e0d3747b250
thiagomessias411@gmail.com <Thiagomessias411@gmail.com>
2025-05-19 10:56:49 -0300

    :zap: Update directory and uploading index.html

--- treinamento.py
+++ treinamento.py
@@ -9,9 +9,12 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 import joblib
 import os
 
+TRAINING_DIR = 'training'
+if not os.path.exists(TRAINING_DIR):
+    os.makedirs(TRAINING_DIR)
+
 def preparar_dados(file_path):
     base = pd.read_csv(file_path)
-
     base['Size'] = base['Size'].astype(str).str.replace(',', '.').astype(float)
 
     X = base[['Weight', 'Size']].values
@@ -19,11 +22,11 @@ def preparar_dados(file_path):
 
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
-    joblib.dump(scaler, 'scaler.pkl')
+    joblib.dump(scaler, os.path.join(TRAINING_DIR, 'scaler.pkl'))
 
     label_encoder = LabelEncoder()
     y = label_encoder.fit_transform(y)
-    joblib.dump(label_encoder, 'label_encoder.pkl')
+    joblib.dump(label_encoder, os.path.join(TRAINING_DIR, 'label_encoder.pkl'))
 
     return X, y
 
@@ -40,16 +43,16 @@ def treinar_modelo(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     model = criar_modelo()
 
-    checkpoint = ModelCheckpoint('model.keras', save_best_only=True, monitor='val_accuracy', mode='max')
+    checkpoint = ModelCheckpoint(os.path.join(TRAINING_DIR, 'model.keras'), save_best_only=True, monitor='val_accuracy', mode='max')
     model.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test), callbacks=[checkpoint])
     loss, accuracy = model.evaluate(X_test, y_test)
     print(f'Acurácia no conjunto de teste: {accuracy * 100:.2f}%')
     return accuracy
 
 def carregar_modelo():
-    model = tf.keras.models.load_model('model.keras')
-    scaler = joblib.load('scaler.pkl')
-    label_encoder = joblib.load('label_encoder.pkl')
+    model = tf.keras.models.load_model(os.path.join(TRAINING_DIR, 'model.keras'))
+    scaler = joblib.load(os.path.join(TRAINING_DIR, 'scaler.pkl'))
+    label_encoder = joblib.load(os.path.join(TRAINING_DIR, 'label_encoder.pkl'))
     return model, scaler, label_encoder
 
 def prever_especie(input_data):
@@ -61,9 +64,9 @@ def prever_especie(input_data):
     return species[0]
 
 if __name__ == '__main__':
-    file_path = 'apples_and_oranges.csv' 
+    file_path = './data/apples_and_oranges.csv'
     X, y = preparar_dados(file_path)
     treinar_modelo(X, y)
-    sample_input = [72, 5.85] 
+    sample_input = [72, 5.85]
     species = prever_especie(sample_input)
     print(f'A fruta prevista para {sample_input} é: {species}')
